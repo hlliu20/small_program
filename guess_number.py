19bbccc51026e07239fbed69fdaffae67851c9fa
hlliu20 <63887028+hlliu20@users.noreply.github.com>
2020-08-30 01:09:17 -0500

    Update guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -1,18 +1,21 @@
 import random
 def guess():
-    min = eval(input('please input the min limitation:'))
-    max = eval(input('please input the max iimitation:'))
+    print("猜数游戏")
+    min = eval(input('请输入最大值:'))
+    max = eval(input('请输入最小值:'))
     guess = random.randint(min, max)
     people = min-1
+    time = 1
     while True:
-        people = eval(input('your guess:'))
+        people = eval(input('你的猜测:'))
         if people ==  guess:
-            print('you got it!')
+            print("恭喜你，猜对了!， 你用了{}次。".format(time))
             break
         elif people > guess:
             print("too big")
         else :
             print("too little")
+        time += 1
 
 
 def main():
